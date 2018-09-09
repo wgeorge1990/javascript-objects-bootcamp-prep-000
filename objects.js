8129b7896ae6bf4a12c7c38925b44e78428f304f
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-09 04:11:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ function updatePlaylist( playlist, artistname, songTitle){
   playlist[artistname]=songTitle
 }
 function removeFromPlaylist(playlist, artistname){
-delete playlist.artistname
+delete playlist[artistname]
 }
